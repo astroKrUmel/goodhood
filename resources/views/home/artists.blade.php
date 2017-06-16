838e50a58dccae19a31ca9ecaeb70f8e9248b733
Kevin <cyclonicmen@googlemail.com>
2017-06-16 22:59:55 +0200

    make home artists section for mobile greater

--- resources/views/home/artists.blade.php
+++ resources/views/home/artists.blade.php
@@ -2,10 +2,10 @@
     <h2 class="sub-headline">ARTISTS</h2>
 
     @foreach($artists as $artist)
-        <div class="picture-collection artist">
+        <a class="picture-collection artist">
             <img src="../../images/artists/{{$artist->urlName}}/main.jpg" width="100%" alt="{{strtoupper($artist->name)}}">
             <strong class="name">{{strtoupper($artist->name)}}</strong>
-        </div>
+        </a>
     @endforeach
 
     <div class="seperator"></div>

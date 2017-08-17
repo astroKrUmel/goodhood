e96c0a71e4b03d7bcad48274c1bffaffd40294d5
Kevin <cyclonicmen@googlemail.com>
2017-08-17 15:10:01 +0200

    make mediaBOx responsive, update mediaBox icons

--- resources/views/artists/mediaBox.blade.php
+++ resources/views/artists/mediaBox.blade.php
@@ -5,6 +5,8 @@
             {!! Helper::createEmbedTemplate($tracks[0]['code'], 'mini') !!}
         @endif
 
+        {{--{{$artist}}--}}
+
         <div class="links">
             @if (strlen($artist->facebookLink) > 0)
                 <a class="link icon icon-facebook" href="{{$artist->facebookLink}}" target="_blank" title="Facebook"></a>
@@ -14,6 +16,11 @@
                 <a class="link icon icon-mixcloud" href="{{$artist->mixcloudLink}}" target="_blank" title="Mixcloud"></a>
             @endif
 
+            @if (strlen($artist->link) > 0)
+                <a class="link direct-link" href="{{$artist->link}}" target="_blank" title="{{$artist->link}}">
+                    {{$artist->link}}
+                </a>
+            @endif
         </div>
     </div>
 </section>

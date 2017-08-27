5e33738595a470d2ce14221ad8cedc3a2726c120
Kevin <cyclonicmen@googlemail.com>
2017-08-27 03:25:23 +0200

    add another dummy partners, update one partner img, add partner styling

--- resources/views/partner.blade.php
+++ resources/views/partner.blade.php
@@ -4,15 +4,18 @@
 
     @include('common.logo')
 
-    <section id="partners">
+    <section id="partner">
         @include('common.navigation')
 
         <h2 class="sub-headline">PARTNERS</h2>
 
-        @foreach ($partners as $partner)
-            <a class="picture-collection partner" href="{{$partner['link']}}" target="_blank">
-                <img src="{{ '../../images/partners/' . $partner['name']}}" width="100%">
-            </a>
-        @endforeach
+        <div class="partner-content">
+            @foreach ($partners as $partner)
+                <a class="picture-collection partner-element" href="{{$partner['link']}}" target="_blank">
+                    <img src="{{ '../../images/partners/' . $partner['name']}}" width="100%">
+                </a>
+            @endforeach
+        </div>
+
     </section>
 @endsection

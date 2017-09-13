c10b4bb84b73d3a41ba8d4562719b049558c3aa4
Kevin <cyclonicmen@googlemail.com>
2017-09-13 21:41:00 +0200

    add partner beyond

--- config/partners.php
+++ config/partners.php
@@ -24,16 +24,8 @@ return [
         'name' => 'buzz-up.png',
         'link' => 'http://www.newdef.de/'
     ],
-//    '5' => [
-//        'name' => 'RadioAktiv2punkt0.png',
-//        'link' => 'https://www.radioaktiv2punkt0.com/'
-//    ],
-//    '6' => [
-//        'name' => 'BeppoSAndPeterB.png',
-//        'link' => 'https://beppoundpeter.bandcamp.com/'
-//    ],
-//    '4' => [
-//        'name' => 'SharpRadio',
-//        'link' => 'https://www.mixcloud.com/SharpRadio/'
-//    ]
+    '5' => [
+        'name' => 'beyond_logo.png',
+        'link' => 'http://beyond07.com/'
+    ]
 ];

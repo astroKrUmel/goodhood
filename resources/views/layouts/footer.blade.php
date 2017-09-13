3879f33fe407198e88e8b1745ce827fd0c28142b
Kevin <cyclonicmen@googlemail.com>
2017-09-13 20:52:42 +0200

    add footer template, remove navi hover effect

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,12 @@
-<section id="footer" style="background: white">
-    footer
+<section id="footer">
+    <nav class="navigation">
+        <ul>
+            <li class="impressum">
+                <a href="/impressum" target="_blank">Impressum</a>
+            </li>
+            <li class="datenschutz">
+                <a href="/datenschutz" target="_blank">Datenschutz</a>
+            </li>
+        </ul>
+    </nav>
 </section>

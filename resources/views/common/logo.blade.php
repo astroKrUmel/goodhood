cf6978d2709854fe064c87eecf4e75d7eff451da
Kevin <cyclonicmen@googlemail.com>
2017-09-13 20:26:22 +0200

    add new artist images

--- resources/views/common/logo.blade.php
+++ resources/views/common/logo.blade.php
@@ -1,7 +1,9 @@
 <section id="logo">
-    <picture class="main-image">
-        <source srcset="../../images/logos/GoodHood-Logo-Mobile.png" media="(max-width: 479px)">
-        <source srcset="../../images/logos/GoodHood-Logo-Desktop.png" media="(min-width: 480px)">
-        <img src="../../images/logos/GoodHood-Logo-Desktop.png" width="100%" alt="Goodhood-Logo-Desktop">
-    </picture>
+    <a href="/home">
+        <picture class="main-image">
+            <source srcset="../../images/logos/GoodHood-Logo-Mobile.png" media="(max-width: 479px)">
+            <source srcset="../../images/logos/GoodHood-Logo-Desktop.png" media="(min-width: 480px)">
+            <img src="../../images/logos/GoodHood-Logo-Desktop.png" width="100%" alt="Goodhood-Logo-Desktop">
+        </picture>
+    </a>
 </section>

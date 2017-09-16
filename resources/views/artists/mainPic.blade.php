f49c32092a5000360e3184b1c501ecd65323d176
Kevin <cyclonicmen@googlemail.com>
2017-09-17 00:36:03 +0200

    set max height for artist image for big tablets and desktop

--- resources/views/artists/mainPic.blade.php
+++ resources/views/artists/mainPic.blade.php
@@ -1,4 +1,7 @@
 <section id="main-pic">
     <h3>{{ ucfirst($artist->name) }}</h3>
-    <img src="../../images/artists/{{$artist->urlName}}/main.jpg" alt="{{$artist->name}}" width="100%">
+    <div class="artist-image">
+        <img src="../../images/artists/{{$artist->urlName}}/main.jpg" alt="{{$artist->name}}" width="100%">
+    </div>
+
 </section>

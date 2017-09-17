b1ff51cd79837ceccb675e6ba0e2bdad7e246c67
Kevin <cyclonicmen@googlemail.com>
2017-09-17 12:32:44 +0200

    update email config, update final navi styling

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,17 +1,3 @@
-{{--<section id="footer">--}}
-    {{--<nav class="navigation">--}}
-        {{--<ul>--}}
-            {{--<li class="impressum">--}}
-                {{--<a href="/impressum" target="_blank">Impressum</a>--}}
-            {{--</li>--}}
-            {{--<li class="datenschutz">--}}
-                {{--<a href="/datenschutz" target="_blank">Datenschutz</a>--}}
-            {{--</li>--}}
-        {{--</ul>--}}
-    {{--</nav>--}}
-{{--</section>--}}
-
-
 <section id="footer">
     <nav class="navigation">
         <ul class="links">
